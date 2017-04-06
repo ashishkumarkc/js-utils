04feab9418b7f7dddb7c83347070a8040b825e77
K.C.Ashish Kumar <kcak11@gmail.com>
2017-04-06 11:59:42 +0530

    Update transformation-algorithm.kcak11.js

--- transformation-algorithm.kcak11.js
+++ transformation-algorithm.kcak11.js
@@ -1,13 +1,13 @@
-var s="This is a sample string for testing purpose by K.C.Ashish Kumar";
-var t="";
-for(var i=0;i<s.length;i++){
-t+=String.fromCharCode(s.charCodeAt(i)^11^((i+1)%2));
+var s = "This is a sample string for testing purpose by K.C.Ashish Kumar";
+var t = "";
+for (var i = 0; i < s.length; i++) {
+    t += String.fromCharCode((s.charCodeAt(i) ^ 11 ^ ((i + 11) % 11)));
 }
-var _t=encodeURIComponent(t);
+var _t = encodeURIComponent(t);
 console.log(_t);
-var t=decodeURIComponent(_t);
-var res="";
-for(var i=0;i<t.length;i++){
-res+=String.fromCharCode(t.charCodeAt(i)^11^((i+1)%2));
+var t = decodeURIComponent(_t);
+var res = "";
+for (var i = 0; i < t.length; i++) {
+    res += String.fromCharCode((t.charCodeAt(i) ^ 11 ^ ((i + 11) % 11)));
 }
 console.log(res);
